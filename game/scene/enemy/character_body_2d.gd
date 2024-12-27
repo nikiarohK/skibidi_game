8c9ad3f8c0a7f55581d2f531320f641bab25facf
aberton228 <aberton.3@mail.ru>
2024-12-27 20:24:01 +0300

    enemy walk try

--- game/scene/enemy/character_body_2d.gd
+++ game/scene/enemy/character_body_2d.gd
@@ -3,18 +3,29 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
-
+var direction = 1
+var walk = true
 func _physics_process(delta: float) -> void:
 
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if walk:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		direction = -direction
+		
 
 	move_and_slide()
+
+
+func _on_wallcheck_body_entered(body: Node2D) -> void:
+	walk = false
+
+
+func _on_floorcheck_body_entered(body: Node2D) -> void:
+	walk = true
+
+
+
+func _on_floorcheck_body_exited(body: Node2D) -> void:
+	walk = false
