37105b038d99c16b4064733dafa813bfcd9f357d
aberton228 <aberton.3@mail.ru>
2024-12-27 15:08:36 +0300

    menu

--- game/levels/menu/menu.gd
+++ game/levels/menu/menu.gd
@@ -1,9 +1,8 @@
 extends Node2D
 
+func _on_play_pressed() -> void:
+	pass # Replace with function body.
+
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-
-
-func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://level.tscn")
